f55f5b49767da0de8f4233c2d0592def950ad2a7
Diego Macario Bello <diego.macariobello@shopify.com>
2025-01-29 12:35:49 -0500

    Add export button.

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -2,14 +2,17 @@ import { useModelStore } from "@/stores/useModelStore";
 import { updateModel } from "@/utils/utils";
 import { OrbitControls, Stage, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { button, useControls } from "leva";
 import { Suspense, useEffect, useRef } from "react";
 import type { OrbitControls as OrbitControlsImpl } from "three-stdlib";
+import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter.js";
 
 interface ModelViewProps {
   url: string;
 }
 
 export default function ModelView({ url }: ModelViewProps) {
+  const sceneRef = useRef();
   const orbitControlsRef = useRef<OrbitControlsImpl>(null);
   const gltf = useGLTF(url);
   const { model, compressionSettings } = useModelStore();
@@ -25,6 +28,43 @@ export default function ModelView({ url }: ModelViewProps) {
     }
   }, [compressionSettings, model]);
 
+  const exportGLTF = () => {
+    if (!sceneRef.current) return;
+
+    const exporter = new GLTFExporter();
+    exporter.parse(
+      sceneRef.current,
+      (gltf) => {
+        // @ts-ignore
+        const blob = new Blob([gltf], { type: "application/octet-stream" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = "scene.glb";
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(url);
+      },
+      (error) => {
+        console.error("An error occurred while exporting the file", error);
+      },
+      {
+        binary: true,
+      }
+    );
+  };
+
+  useControls(
+    "Export",
+    {
+      Export: button(() => {
+        exportGLTF();
+      }),
+    },
+    { collapsed: false }
+  );
+
   return (
     <div id="view-3d">
       <Canvas camera={{ position: [0, 0, 150], fov: 50 }}>
@@ -38,7 +78,7 @@ export default function ModelView({ url }: ModelViewProps) {
             adjustCamera
             environment={"city"}
           >
-            <primitive object={gltf.scene} />
+            <primitive ref={sceneRef} object={gltf.scene} />
           </Stage>
         </Suspense>
         <OrbitControls ref={orbitControlsRef} />
