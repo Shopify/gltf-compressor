4f6e947be2c82e0a94f904c831bead674e7cdd4d
Daniel Beauchamp <daniel.beauchamp@gmail.com>
2025-01-29 22:23:21 -0500

    Shorted stats names

--- src/components/StatsView.tsx
+++ src/components/StatsView.tsx
@@ -83,20 +83,21 @@ export default function StatsView({ url }: StatsViewProps) {
 
   return (
     <div className="absolute top-4 left-4 p-4 text-white font-mono text-xs select-none pointer-events-none">
-      <div>Number of Meshes: {stats.numMeshes}</div>
-      <div>Number of Vertices: {stats.numVertices}</div>
-      <div>Number of Textures: {stats.numTextures}</div>
-      <div>Number of Animation Clips: {stats.numAnimationClips}</div>
+      <div>Meshes: {stats.numMeshes}</div>
+      <div>Verts: {stats.numVertices}</div>
+      <div>Textures: {stats.numTextures}</div>
+      <div>Anim Clips: {stats.numAnimationClips}</div>
+      <br />
       <div>
-        Size of Meshes: {stats.sizeOfMeshes.toFixed(2)} KB (
+        Meshes: {stats.sizeOfMeshes.toFixed(2)} KB (
         {stats.percentOfSizeTakenByMeshes.toFixed(2)}%)
       </div>
       <div>
-        Size of Textures: {stats.sizeOfTextures.toFixed(2)} KB (
+        Textures: {stats.sizeOfTextures.toFixed(2)} KB (
         {stats.percentOfSizeTakenByTextures.toFixed(2)}%)
       </div>
       <div>
-        Size of Animation Clips: {stats.sizeOfAnimations.toFixed(2)} KB (
+        Anim Clips: {stats.sizeOfAnimations.toFixed(2)} KB (
         {stats.percentOfSizeTakenByAnimations.toFixed(2)}%)
       </div>
     </div>
