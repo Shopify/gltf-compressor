8aa65562c6cc5e0faaa7787ce02d214c71bbaf81
Diego Macario Bello <diego.macariobello@shopify.com>
2025-02-17 12:40:29 -0500

    Remove tailwind.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
@@ -6,7 +5,7 @@ import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), glsl()],
+  plugins: [react(), glsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
