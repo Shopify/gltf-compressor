b5edfe34d2abc1f53ca6fd39f59ad9010a01d569
Diego Macario Bello <diego.macariobello@shopify.com>
2025-02-17 13:04:27 -0500

    Add shadcn/ui dependency.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), glsl()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
