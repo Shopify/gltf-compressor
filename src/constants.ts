c6b2ba471810ba56d4d2e674e12e33bf79389f86
Diego Macario Bello <diego.macariobello@shopify.com>
2025-02-17 15:42:43 -0500

    Remove stuff related to Leva.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,1 @@
 export const defaultTextureQuality = 80;
-
-/* --- Leva Folder Order ---- */
-
-export const VIEWPORT_FOLDER_ORDER = 1;
-export const MATERIAL_FOLDER_ORDER = 2;
-export const EXPORT_FOLDER_ORDER = 3;
--- src/plugins/plugin-tree/index.ts
+++
@@ -1,9 +0,0 @@
-import { createPlugin } from "leva/plugin";
-import { Tree } from "./Tree.tsx";
-import { normalize, sanitize } from "./tree-plugin.ts";
-
-export const tree = createPlugin({
-  normalize,
-  sanitize,
-  component: Tree,
-});
